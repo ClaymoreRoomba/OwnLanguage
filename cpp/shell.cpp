89808c4675af3c22334f65fde25c686a011d5aec
Filip <filip.keerberg@gmail.com>
2022-08-06 17:47:55 +0200

    Adding the c++ files

---
+++ cpp/shell.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    while(true){
+
+        cout << "basic > ";
+        string input;
+        getline(cin, input);
+        
+        cout << input << endl; 
+
+    }
+}
